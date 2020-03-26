78fa5b005c0e20a9bd67ad25a77dacb8c7a129bc
Nicole <niohres@gmail.com>
2020-03-26 11:30:15 -0300

    Ofense and defense stats

---
+++ Assets/Scripts/NavGame/Core/DefenseStats.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NavGame.Core
+{
+
+    [Serializable]
+    public class DefenseStats
+    {
+        public int maxHealth = 100;
+        
+        public int armor = 1;
+        
+
+    }
+}
+
---
+++ Assets/Scripts/NavGame/Core/OfenseStats.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NavGame.Core
+{
+
+    [Serializable]
+    public class OfenseStats
+    {
+       
+        public int damage = 10;
+      
+        public float attackSpeed = 1f;
+
+    }
+}
+
